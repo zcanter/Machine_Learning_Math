2a4db1795f30e610e2f4683359dfb10dcab4a14d
Zac Canter <zrcanter@gmail.com>
2019-08-14 12:19:37 -0700

    Update LAutil.py

--- LAutil.py
+++ LAutil.py
@@ -30,3 +30,16 @@ def refection_matrix(A):
     R = E@T@transpose(E)
     return R
 
+# PageRank Algorithm (linkMatrix is the initial probabilities of page network, d is dampening factor -> good to try d=0.5)
+def pageRank(linkMatrix, d):
+    n = linkMatrix.shape[0]
+    M = d * linkMatrix + (1-d)/n * np.ones([n, n])
+    r = 100*np.ones(n)/n
+    lastR = r
+    r = M @ r
+    i = 0
+    while la.norm(lastR - r) > 0.01 :
+        lastR = r
+        r = M @ r
+        i += 1
+    print(str(i) + " iterations to convergence.")
