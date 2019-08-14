38f92327308a246b45eaf74209cd41ad54967e33
Zac Canter <zrcanter@gmail.com>
2019-08-14 10:24:37 -0700

    Update util.py

--- util.py
+++ util.py
@@ -4,7 +4,7 @@
 import numpy as np
 import numpy.linalg as la
 
-# Function which uses Gram-schmidt process to find bases (adapted from Coursera Math for Machine Learning)
+# Outputs orthonormal basis for some matrix using Gramm-Schmitt
 def gsBasis(A) :
     B = np.array(A, dtype=np.float_) 
     for i in range(B.shape[1]) :
@@ -16,7 +16,17 @@ def gsBasis(A) :
             B[:, i] = np.zeros_like(B[:, i])
     return B
 
-# Function which uses the Gram-schmidt process to calculate the dimension
+# Outputs dimensions of an orthonormal basis using Gramm-Schmitt
 def dimensions(A) :
     return np.sum(la.norm(gsBasis(A), axis=0))
+
+# Outputs matrix reflected about some orthonormal basis vectors also uses Gramm-Schmitt
+def refection_matrix(A):
+    # get basis via Gramm Schmitt
+    E = gsBasis(A)
+    # desired reflection (about y-axis)
+    T =  np.array([[1, 0], [0, -1]])
+    # formula to output
+    R = E@T@transpose(E)
+    return R
 
