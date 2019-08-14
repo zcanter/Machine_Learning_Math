420132470671a81799f4af0106a1b1b493e74a0c
Zac Canter <zrcanter@gmail.com>
2019-08-14 09:11:11 -0700

    Update util.py

--- util.py
+++ util.py
@@ -1,7 +1,22 @@
 
-# Written by Zac Canter
+# Written by Zac Canter (many adapted from Coursera's Mathematics from Machine Learnine)
 
 import numpy as np
+import numpy.linalg as la
 
-# Function to determine if a 4x4 matrix is singular (i.e. has an inverse)
+# Function which uses Gram-schmidt process to find bases (adapted from Coursera Math for Machine Learning)
+def gsBasis(A) :
+    B = np.array(A, dtype=np.float_) 
+    for i in range(B.shape[1]) :
+        for j in range(i) :
+            B[:, i] = B[:, i] - B[:, i] @ B[:, j] * B[:, j]
+        if la.norm(B[:, i]) > 1e-14:
+            B[:, i] = B[:, i] / la.norm(B[:, i])
+        else :
+            B[:, i] = np.zeros_like(B[:, i])
+    return B
+
+# Function which uses the Gram-schmidt process to calculate the dimension
+def dimensions(A) :
+    return np.sum(la.norm(gsBasis(A), axis=0))
 
